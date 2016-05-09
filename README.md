ce292fe020cedd7498bdf46072659f96ae629d0f
lyckade <as@lycka.de>
2016-05-09 21:17:11 +0200

    First structure for the handlers

--- README.md
+++ README.md
@@ -11,8 +11,7 @@ There is a client and a server communicating together. the gonetsync instance wo
 Just a short description how this very simple API works between the client and the server.
 
 * Client sends a GET to the server with the filepath
-  * Path: /server/file/
-* Server answers if file exists. If the file already exists on the server the fileInfo is sent, too.
-  * Path: /client/file/
+  * Path: /server/file/{package}?filepath=...
+  * Server answers if file exists or not. If the file already exists on the server the fileInfo is sent, too.
 * Client decides to send or not to send the file via PUT
-  * Path /client/file/
+  * Path /client/file/{package}/{filepath}
--- main.go
+++ main.go
@@ -11,19 +11,22 @@ import (
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/server/file", ServerFileGET).Methods("GET")
-	router.HandleFunc("/server/file", ServerFilePUT).Methods("PUT")
-	router.HandleFunc("/client/file", ClientFileGET).Methods("GET")
-
+	router.HandleFunc("/server/file/{package}", ServerFileGET).Methods("GET")
+	router.HandleFunc("/server/file/{package}", ServerFilePUT).Methods("PUT")
+	router.HandleFunc("/client/file/{package}", ClientFileGET).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8081", router))
+
 }
 
 //ServerFileGET is a request to the server to send informations
 //about a file. If the File does exist a os.FileInfo is sent
 //back to the client.
 func ServerFileGET(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%#v", r.Body)
-	fmt.Fprintln(w, "Here is the file")
+	vars := mux.Vars(r)
+
+	fmt.Fprintf(w, "Package: %#v; Filepath:%s", vars["package"], r.FormValue("filepath"))
+
+	//fmt.Fprintln(w, "No")
 }
 
 //ServerFilePUT sends a file to the server to store it
