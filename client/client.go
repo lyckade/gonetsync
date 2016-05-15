9dad2be4ffb76e94b077dbfe5ad9c1636a81db97
lyckade <as@lycka.de>
2016-05-15 07:37:08 +0200

    refactoring the client

--- client/client.go
+++ client/client.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/lyckade/golib/mylogger"
@@ -29,43 +30,45 @@ func folderWalk(baseFolder string) {
 }
 
 func walkFunc(baseFolder string, fpath string, info os.FileInfo, err error) error {
-	//fmt.Printf("%v\n", fpath)
 
-	//fmt.Printf("%v\n\n", info.ModTime())
 	if info.IsDir() != true {
-
-		modTime := info.ModTime()
-		ts := modTime.Format(timestampLayout)
+		ts, _ := makeTimestamp(info)
 		fmt.Println("Timestamp: ", ts)
 
-		urlStr := "http://localhost:8081"
+		urlStr := myConf.ServerAdress
 		packageStr := "meinPacket"
 
+		//Path transformation
 		fpath = filepath.Clean(fpath)
 		relPath, _ := filepath.Rel(baseFolder, fpath)
 		filePath := filepath.ToSlash(relPath)
+		urlStr = strings.Join([]string{
+			urlStr,
+			packageStr,
+			filePath}, "/")
 
-		fmt.Printf("\nfilePath: %v\n%v\n\n", filePath, fpath)
+		//File reading
 		fileReader, err1 := os.Open(fpath)
 		defer fileReader.Close()
 		if err1 != nil {
 			myLogger.Println("Error opening file ", fpath)
 			myLogger.Println(err)
 		}
-		urlStr = strings.Join([]string{
-			urlStr,
-			packageStr,
-			filePath}, "/")
-		fmt.Printf("URLSTR: %v\n", urlStr)
+
 		r, err2 := http.NewRequest("PUT", urlStr, fileReader)
 		if err2 != nil {
 			myLogger.Println(err)
 		}
-		fmt.Println(r)
+
 		client := new(http.Client)
 		resp, err3 := client.Do(r)
 		fmt.Println("RESPONSE: ", resp)
 		fmt.Println("RSPERR: ", err3)
 	}
 	return err
+}
+
+func makeTimestamp(info os.FileInfo) (int, error) {
+	ts := info.ModTime().Format(timestampLayout)
+	return strconv.Atoi(ts)
 }
