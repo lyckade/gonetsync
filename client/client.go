e47d4a0b3aaab1b8cc38cdd25f3fc597c4edcd9d
lyckade <as@lycka.de>
2016-05-16 10:08:51 +0200

    #11 url generation as func

--- client/client.go
+++ client/client.go
@@ -4,10 +4,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
-	"strings"
 
 	"github.com/lyckade/golib/mylogger"
 )
@@ -35,30 +35,14 @@ func walkFunc(baseFolder string, fpath string, info os.FileInfo, err error) erro
 		ts, _ := makeTimestamp(info)
 		fmt.Println("Timestamp: ", ts)
 
-		urlStr := myConf.ServerAdress
 		packageStr := "meinPacket"
 
 		//Path transformation
 		fpath = filepath.Clean(fpath)
-		relPath, _ := filepath.Rel(baseFolder, fpath)
-		filePath := filepath.ToSlash(relPath)
-		urlStr = strings.Join([]string{
-			urlStr,
-			packageStr,
-			filePath}, "/")
 
-		//File reading
-		fileReader, err1 := os.Open(fpath)
-		defer fileReader.Close()
-		if err1 != nil {
-			myLogger.Println("Error opening file ", fpath)
-			myLogger.Println(err)
-		}
+		url := makeURL(myConf.ServerAdress, packageStr, baseFolder, fpath)
 
-		r, err2 := http.NewRequest("PUT", urlStr, fileReader)
-		if err2 != nil {
-			myLogger.Println(err)
-		}
+		r := makeFileRequest(fpath, url.String())
 
 		client := new(http.Client)
 		resp, err3 := client.Do(r)
@@ -71,4 +55,30 @@ func walkFunc(baseFolder string, fpath string, info os.FileInfo, err error) erro
 func makeTimestamp(info os.FileInfo) (int, error) {
 	ts := info.ModTime().Format(timestampLayout)
 	return strconv.Atoi(ts)
+}
+
+func makeURL(schemeHost string, packageStr string, baseFolder string, filePath string) url.URL {
+	url, err := url.Parse(schemeHost)
+	if err != nil {
+		myLogger.Print(err)
+	}
+	relPath, _ := filepath.Rel(baseFolder, filePath)
+	url.Path = packageStr + "/" + filepath.ToSlash(relPath)
+	return *url
+}
+
+func makeFileRequest(fpath string, urlStr string) *http.Request {
+	//File reading
+	fileReader, err1 := os.Open(fpath)
+	defer fileReader.Close()
+	if err1 != nil {
+		myLogger.Println("Error opening file ", fpath)
+		myLogger.Println(err1)
+	}
+
+	r, err2 := http.NewRequest("PUT", urlStr, fileReader)
+	if err2 != nil {
+		myLogger.Println(err2)
+	}
+	return r
 }
--- client/client_test.go
+++ client/client_test.go
@@ -10,7 +10,12 @@ func TestMain(t *testing.T) {
 
 	Convey("fileWalk", t, func() {
 		//folderWalk(conf.Client.SyncFolder)
-		folderWalk("C:\\Users\\Schroepfer\\test\\LayoutSemantic")
+		//folderWalk("C:\\Users\\Schroepfer\\test\\LayoutSemantic")
+	})
+
+	Convey("CreateUrl", t, func() {
+		url := makeURL("http://local:1234", "p1", "a/b", "a/b/c/file.txt")
+		So(url.String(), ShouldEqual, "http://local:1234/p1/c/file.txt")
 	})
 
 }
